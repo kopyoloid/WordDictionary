84d0d3706adf3c615d97d98cf097c127bfa730b2
kopyoloid <51232482+kopyoloid@users.noreply.github.com>
2021-02-15 16:39:51 -0800

    Add files via upload

---
+++ WordDictionary/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  WordDictionary
+//
+//  Created by kopyoloid on 15/02/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var Searchinput: UITextField!
+    @IBOutlet weak var searchon: UIButton!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
